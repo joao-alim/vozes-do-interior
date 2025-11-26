fde1eec501eb00c2dd70998fb630c4bfc789431b
joão <whereizj@gmail.com>
2025-11-26 16:17:07 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ArtistasComponent } from './pages/artistas/artistas.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'artistas', component: ArtistasComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: '**', redirectTo: '' } 
+];
---
+++ src/app/components/artist-card/artist-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-card',
+  imports: [],
+  templateUrl: './artist-card.component.html',
+  styleUrl: './artist-card.component.css'
+})
+export class ArtistCardComponent {
+
+}
